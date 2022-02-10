838210f7af3109d703c096415202ff529b29a5c1
Tadd E. Dawson <tadd@microsoft.com>
2022-02-10 16:22:19 -0500

    path to terminal settings file

--- Set-Accessibility.ps1
+++ Set-Accessibility.ps1
@@ -2,7 +2,9 @@
     .SYNOPSIS
         Setting up the terminal to be accessible in presentations
         GOAL :  Meet WCAG Level AAA. WCAG Level AAA requires a contrast ratio of at least 7:1 for normal text and 4.5:1 for large text.
-                Large text is defined as 14 point (typically 18.66px) and bold or larger, or 18 point (typically 24px) or larger.
+        Large text is defined as 14 point (typically 18.66px) and bold or larger, or 18 point (typically 24px) or larger.
+        Windows Terminal settings.json if it gets out of wack
+        C:\Users\tadd\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
     .EXAMPLE
         .\Set-Accessibility.ps1
 #>
